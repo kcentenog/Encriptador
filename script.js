a5869bfea42af8468315625e7ab8b6657fe05d02
kcentenog <kcentenog@outlook.com>
2024-03-18 15:40:42 -0600

    Error encriptar, no funciona copiar

--- script.js
+++ script.js
@@ -62,20 +62,26 @@ function desencriptarTexto(mensaje){
     var textoFinal = "";
 
     for(var i = 0; i < texto.length; i++){
-        if(texto[i] == "ai"){
+        if(texto[i] == "a"){
+            textoFinal = textoFinal + "a"
+            i = i+1;
         }
-        else if(texto[i] == "enter"){
+        else if(texto[i] == "e"){
             textoFinal = textoFinal + "e"
+            i = i+4;
         }
-        else if(texto[i] == "imes"){
+        else if(texto[i] == "i"){
             textoFinal = textoFinal + "i"
+            i = i+3;
         }
-        else if(texto[i] == "ober"){
+        else if(texto[i] == "o"){
             textoFinal = textoFinal + "o"
+            i = i+3;
         }
         
-        else if(texto[i] == "ufat"){
+        else if(texto[i] == "u"){
             textoFinal = textoFinal + "u"
+            i = i+3;
         }
         else{
             textoFinal = textoFinal + texto[i];
@@ -86,6 +92,7 @@ function desencriptarTexto(mensaje){
     return textoFinal;
 
 }
+
 
 const btnCopiar = document.querySelector(".copiar"); 
     btnCopiar.addEventListener("click", copiar = () => {
